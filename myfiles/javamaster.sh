fc1112f496a293a8aa1f4e36aba0f5d795606e2a
Nikhil-tr <152260396+Nikhil-tr@users.noreply.github.com>
2024-03-14 15:03:53 +0530

    Add files via upload

---
+++ myfiles/javamaster.sh
@@ -0,0 +1,17 @@
+#JAVA_11
+sudo add-apt-repository ppa:openjdk-r/ppa -y
+sudo apt-get update -y
+sudo apt-get install -y fontconfig openjdk-11-jre openjdk-11-jdk
+
+#JENKIN
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+
+#DOCKER
+sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/scripts/installDocker.sh -P /tmp
+sudo chmod 755 /tmp/installDocker.sh
+sudo bash /tmp/installDocker.sh
+sudo systemctl restart docker.service
+sudo usermod -aG docker ubuntu   
